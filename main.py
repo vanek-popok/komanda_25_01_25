126f172d0c07621c183f483092c7935267fe948d
vanek-popok <veno2287772@gmail.com>
2025-02-08 11:43:17 +0300

    ivan

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 
 import telebot
 
+from ivan import ivanvan
+
 bot = telebot.TeleBot("7333117946:AAFa98LJs9CJu17eTM7pLB-mVmkWfqA_5Co")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Привет")
+
+@bot.message_handler(commands=['ivan'])
+def ivan(message):
+    ivanvan(message,bot)
+
+
+
+
 
 
