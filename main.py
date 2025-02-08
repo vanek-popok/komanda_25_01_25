3ab2b5bd0e2760304cc142e3ecd9cd75b257aaaa
Sanya <aleksandrkorosupov@gmail.com>
2025-02-08 17:43:17 +0900

    qwozyyyfunc

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 import telebot
-
+from qwozyyy import qwozyyyFunc
+from teebot import types
 bot = telebot.TeleBot("7333117946:AAFa98LJs9CJu17eTM7pLB-mVmkWfqA_5Co")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Привет")
 
-
+@bot.message_handler(commands=['qwozyyy'])
+def qwozyyy(message):
+    qwozyyyFunc(message, bot, types)
